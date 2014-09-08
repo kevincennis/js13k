0591a08e28a765c4b50eabefbcf832f426e2c00f
mike <mhelgeson@aereo.com>
2014-09-07 22:50:45 -0400

    more complex collisions

--- src/js/physic.js
+++ src/js/physic.js
@@ -24,23 +24,34 @@ var Physics = {
           if ( Physics.bodies[j] != null ){
             obj2 = Physics.bodies[j];
             // collision detected...
-            if ( Physics.overlap( obj1, obj2 ) === true ){
-              // if ( obj1.type == 'air' && obj2.type == 'fire' ){
-              //   obj1.type = 'fire';
-              //   obj2.destruct();
-              // }
-              // if ( obj1.type == 'fire' && obj2.type == 'air' ){
-              //   obj2.type = 'fire';
-              //   obj2.destruct();
-              // }
-              // if ( obj1.type == 'water' && obj2.type == 'fire' ){
-              //   obj1.destruct();
-              //   obj2.destruct();
-              // }
-              // if ( obj1.type == 'fire' && obj2.type == 'water' ){
-              //   obj1.destruct();
-              //   obj2.destruct();
-              // }
+            var overlapping = Physics.overlap( obj1, obj2 );
+            if (  overlapping > 1 ){
+
+              var depth = 38/4;
+
+              switch ( obj1.mask | obj2.mask ){
+                case FIRE | WATER:
+                case AIR | EARTH:
+                  obj1.set({ r: obj1.r - depth });
+                  obj2.set({ r: obj2.r - depth });
+                  break;
+                case FIRE | AIR:
+                  obj1.set({ r: obj1.r +( obj1.mask & FIRE ? +depth : -depth ) });
+                  obj2.set({ r: obj2.r +( obj2.mask & FIRE ? +depth : -depth ) });
+                  break;
+                case EARTH | FIRE:
+                  obj1.set({ r: obj1.r +( obj1.mask & EARTH ? +depth : -depth ) });
+                  obj2.set({ r: obj2.r +( obj2.mask & EARTH ? +depth : -depth ) });
+                  break;
+                case WATER | EARTH:
+                  obj1.set({ r: obj1.r +( obj1.mask & WATER ? +depth : -depth ) });
+                  obj2.set({ r: obj2.r +( obj2.mask & WATER ? +depth : -depth ) });
+                  break;
+                case AIR | WATER:
+                  obj1.set({ r: obj1.r +( obj1.mask & AIR ? +depth : -depth ) });
+                  obj2.set({ r: obj2.r +( obj2.mask & AIR ? +depth : -depth ) });
+                  break;
+              }
               Physics.impulse( obj1, obj2 );
             }
           }
@@ -53,47 +64,52 @@ var Physics = {
   },
   // do two circles overlap?
   overlap: function( a, b ){
+      if ( !a.active || !b.active ){
+        return false;
+      }
       var r = a.r + b.r,
       x = a.pos.x - b.pos.x,
       y = a.pos.y - b.pos.y;
-      return r * r > x * x + y * y;
+      return ( r * r ) / ( x * x + y * y );
   },
   // http://en.wikipedia.org/wiki/Inelastic_collision#Formula
   impulse: function( a, b ) {
 
-
-
-    // Calculate relative velocity
-
-    var pos_norm = a.pos.clone().sub( b.pos ).norm();
-
+    // vector direction between centers
+    var pos_norm = a.pos.clone().sub( b.pos ).norm(),
     // Calculate relative velocity in terms of the normal direction
-    var velAlongNormal = pos_norm.dot( a.vel.clone().sub( b.vel ) );
+    velAlongNormal = pos_norm.dot( a.vel.clone().sub( b.vel ) ),
+    // Calculate restitution
+    e = ( a.rest + b.rest ) / 2, // avg
+    // Calculate impulse scalar
+    j = -( 1 + e ) * velAlongNormal / ( a.inv_mass + b.inv_mass );
+    // add to acceleration, as change in velocity
+    impulse = pos_norm.clone().mult( j ),
+    // how much overlap...
+    penetration = a.r + b.r - a.pos.clone().sub( b.pos ).length(),
+    percent = 0.5, // usually 20% to 80%
+    slop = 1, // usually 0.01 to 0.1
+    depth = Math.max( penetration - slop, slop )*2;
 
     // Do not resolve if velocities are separating
     if ( velAlongNormal > 0 ){
       return;
     }
-    // Calculate restitution
-    var e = Math.min( a.rest, b.rest );
 
-    // Calculate impulse scalar
-    var j = -( 1 + e ) * velAlongNormal;
-    j /= a.inv_mass + b.inv_mass;
-
-    // Apply impulse
-    var impulse = pos_norm.clone().mult( j );
+    // apply (repelling) impulse
     a.vel.add( impulse.clone().mult( a.inv_mass ) );
     b.vel.sub( impulse.clone().mult( b.inv_mass ) );
-    // position correction for overlaps
-    var percent = 0.5, // usually 20% to 80%
-    slop = 0.1, // usually 0.01 to 0.1
-    penetration = a.r + b.r - a.pos.clone().sub( b.pos ).length(),
-    depth = Math.max( penetration - slop, slop ),
-    correction = pos_norm.mult( ( depth / ( a.inv_mass + b.inv_mass ) ) * percent );
 
-    a.force.add( correction.clone().mult( a.inv_mass ) );
-    b.force.sub( correction.clone().mult( b.inv_mass ) );
+    // apply (attracting) impulse
+    // a.vel.sub( impulse.clone().mult( a.inv_mass ) );
+    // b.vel.add( impulse.clone().mult( b.inv_mass ) );
+
+    // position correction for overlaps
+    //
+    // correction = pos_norm.mult( ( depth / ( a.inv_mass + b.inv_mass ) ) * percent );
+
+    // a.force.add( correction.clone().mult( a.inv_mass ) );
+    // b.force.sub( correction.clone().mult( b.inv_mass ) );
 
   }
 
@@ -148,11 +164,10 @@ var Physic = subclass({
   mass: null, // mass ( volume * density ) calculated
 
   // world registration
-  construct: function( type, x, y, r ){
-    this.type = type;
+  construct: function( type, x, y, r, active ){
+    this.mask |= type;
     this.r = r || this.r;
-    this.mass = Math.PI * this.r * this.r * this.dens;
-    this.inv_mass = 1 / this.mass;
+    this.set( props[ type ] );
     // initialize vectors
     this.pos = new Vect( x, y );
     this.vel = new Vect(0,0);
@@ -160,16 +175,26 @@ var Physic = subclass({
     this.force = new Vect(0,0);
     this.init.apply( this, arguments );
     this.index = Physics.bodies.push( this ) - 1;
+    this.active = active || false;
+  },
+  set: function( opts ){
+    extend.call( this, opts );
+    this.mass = Math.PI * this.r * this.r * this.dens;
+    this.inv_mass = 1 / this.mass;
   },
   // no-op, override in subclasses
   init: function(){ },
 
   // move the properties a step of time (+/-)
   step: function( dt ){
+    if ( this.r < this.min ){
+      this.destruct();
+    }
+
     // the object is moving...
     if ( this.vel.dot() > 0 ){
       // apply a change in velocity due to friction
-      this.vel.mult( 1-this.fric ).min( 1e-3 );
+      // this.vel.mult( 1-this.fric ).min( 1e-3 );
       // calculate the change in position
       this.pos.add( this.vel.clone().mult( dt ) );
     }
@@ -181,15 +206,18 @@ var Physic = subclass({
 
   // collide with the world boundaries
   worlds_collide: function(){
+    if ( !this.active ){
+      return;
+    }
     // north
     if ( this.pos.y < this.r ){
       this.vel.y = Math.abs( this.vel.y );
       this.force.y = this.r - this.pos.y;
     }
     // south
-    if ( this.pos.y > Physics.height - this.r ){
+    if ( this.pos.y > Physics.height - 200 - this.r ){
       this.vel.y = -Math.abs( this.vel.y );
-      this.force.y = Physics.height - this.r - this.pos.y;
+      this.force.y = Physics.height - 200 - this.r - this.pos.y;
     }
     // east
     if ( this.pos.x > Physics.width - this.r ){
@@ -231,4 +259,3 @@ var Physic = subclass({
     delete Physics.bodies[ this.index ];
   }
 });
-
